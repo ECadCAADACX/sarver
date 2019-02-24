72166fef3805e1c2d8d295a3359167e30eee013d
ECadCAADACX <37225682+ECadCAADACX@users.noreply.github.com>
2019-02-24 21:20:48 +0300

    Add files via upload

---
+++ yup.js
@@ -0,0 +1,36 @@
+const Discord = require('discord.js');
+const prefix = `-`
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+
+
+client.on('message', message => {
+   let embed = new Discord.RichEmbed()
+
+    let args = message.content.split(' ').slice(1).join(' ');
+     if(!message.channel.guild) return;
+if(message.content.split(' ')[0] == '-bc') {
+         message.react("✔️")
+          let embed = new Discord.RichEmbed()
+    .setColor("#FF00FF")
+    .setThumbnail(message.author.avatarURL)   
+                                      .addField('تم الارسال بواسطة :', "<@" + message.author.id + ">")
+                 message.channel.sendEmbed(embed);
+        message.guild.members.forEach(m => {
+            var bc = new Discord.RichEmbed()
+.addField('**● Sender  :**', `*** → ${message.author.username}#${message.author.discriminator}***`)
+            .addField('***● Server  :***', `*** → ${message.guild.name}***`)               
+    .setColor('#ff0000')
+                 .addField('ّ', args)
+            m.send(``,{embed: bc});
+        });
+    }
+})
+
+
+client.login('NTQ5MjM2MTY3ODUwMTk3MDEy.D1Q8kw.fH6dijkeZtukLsZOwQB4qmAP7OU');
